7c83594199994a7bb7485f07bdcaca7c35b60f15
Sergey Gorbunov <mongant.s@gmail.com>
2019-02-07 23:05:04 +0200

    hotfix main method

--- src/com/homework/MainApp.java
+++ src/com/homework/MainApp.java
@@ -1,12 +1,16 @@
 package com.homework;
 
-import com.homework.sort.BubbleSort;
+import com.homework.sort.*;
 import com.homework.utils.RandomGenerator;
+import java.util.Arrays;
 
 public class MainApp {
 
     public static void main(String[] args) {
+        MergeSort mergeSort = new MergeSort(RandomGenerator.randomArr(10));
+        mergeSort.sort();
+        System.out.println(Arrays.toString(mergeSort.getSourceArray()));
         BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.sort(RandomGenerator.randomArr(10));
+        bubbleSort.sort(RandomGenerator.randomArr(20));
     }
 }
